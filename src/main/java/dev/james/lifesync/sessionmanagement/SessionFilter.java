14aa23728cdbe7ac342ba3802b701042a4d26f55
James Hillyard <james.hillyard@payara.fish>
2023-10-18 23:53:24 +0100

    Servlet Session Filter
    Signed-off-by: James Hillyard <james.hillyard@payara.fish>

---
+++ src/main/java/dev/james/lifesync/sessionmanagement/SessionFilter.java
@@ -0,0 +1,55 @@
+package dev.james.lifesync.sessionmanagement;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * @author James Hillyard
+ * <p>
+ * This Filter is used to ensure a user has a session when trying to access pages in the LifeSync application.
+ * <p>
+ * This filter excludes `/login` and `/login.jsp` to prevent a too many redirects error, as these pages don't require
+ * a session to be accessed. This is hardcoded and would need expanding if there are more pages that do not require a
+ * session
+ */
+@WebFilter(filterName = "SessionFilter", urlPatterns = "/*")
+public class SessionFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        // Don't session check the login page.
+        String requestURI = httpRequest.getRequestURI();
+        if (requestURI.endsWith("/login") || requestURI.endsWith("/login.jsp")) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        HttpSession session = httpRequest.getSession(false); // Do not create a new session if it doesn't exist
+        if (session != null && session.getAttribute("user") != null) {
+            // User has a session, proceed with the request
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            // No session, redirect to the login servlet
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        // No cleanup required
+    }
+}
