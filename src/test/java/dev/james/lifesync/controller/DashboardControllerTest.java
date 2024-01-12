a98645fe6a4e4c9b9e7afb0fee8d22635b1406c8
James Hillyard <james.hillyard@payara.fish>
2024-01-12 22:19:43 +0000

    Dashboard Controller Tests
    Signed-off-by: James Hillyard <james.hillyard@payara.fish>

---
+++ src/test/java/dev/james/lifesync/controller/DashboardControllerTest.java
@@ -0,0 +1,26 @@
+package dev.james.lifesync.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(DashboardController.class)
+@AutoConfigureMockMvc
+class DashboardControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void showDashboardPage() throws Exception {
+        mockMvc.perform(get("/hlsp/dashboard"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hlsp/dashboard"));
+    }
+}
