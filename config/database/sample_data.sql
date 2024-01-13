4929b4f77f33da1878b97181aff0d9513f3c4b3f
James Hillyard <james.hillyard@payara.fish>
2024-01-13 20:02:38 +0000

    Add Safe User Whose Password Never Changes
    Signed-off-by: James Hillyard <james.hillyard@payara.fish>

--- config/database/sample_data.sql
+++ config/database/sample_data.sql
@@ -1,4 +1,5 @@
 USE lifesync_database;
+INSERT INTO `lifesync_database`.`LifeSyncUser` (`id`, `firstname`, `lastname`, `username`, `password`) VALUES (1, 'James', 'Hillyard', 'jhillyardSAFE', 'test'); # This user never has their credentials changed and has the dummy data below
 INSERT INTO `lifesync_database`.`LifeSyncUser` (`firstname`, `lastname`, `username`, `password`) VALUES ('James', 'Hillyard', 'jhillyard', 'test');
 INSERT INTO `lifesync_database`.`SleepData` (`userid`, `starttime`, `endtime`) VALUES ('1', '2023-10-23 22:00:00', '2023-10-24 08:00:00');
 INSERT INTO `lifesync_database`.`SleepData` (`userid`, `starttime`, `endtime`) VALUES ('1', '2023-10-24 23:30:00', '2023-10-25 09:00:00');
